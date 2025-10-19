57ba6fefa8f15e96ff38da2f53b8c03231ab95c1
Priyanka Pillai <priyankapillai127@gmail.com>
2025-10-18 20:22:58 -0700

    fix/removeComments

--- src/components/TiltCard.tsx
+++ src/components/TiltCard.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useRef } from "react";
 import VanillaTilt from "vanilla-tilt";
 
-// Narrow types for the instance VanillaTilt attaches to the element
 type VanillaTiltInstance = { destroy: () => void };
 type TiltDiv = HTMLDivElement & { vanillaTilt?: VanillaTiltInstance };
 
